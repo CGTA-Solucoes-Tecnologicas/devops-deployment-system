52f21dc4cf90f7a16b9f7be6decfcd1ad0276272
Cireneu Araujo <cireneuguilhermeteixeira@gmail.com>
2025-08-06 23:38:17 -0300

    fixing port

--- backend/index.js
+++ backend/index.js
@@ -85,7 +85,7 @@ app.get('/metrics', (req, res) => {
 module.exports = app;
 
 if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
